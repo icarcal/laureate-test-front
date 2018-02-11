b3a1cbf908067fcf28f8fef3dddaa18a6987c6c1
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-02-11 03:18:06 -0200

    add uber enviromental variables

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,2 +1,4 @@
 export const API_URL = process.env.API_URL;
 export const SEARCH_RADIUS = process.env.SEARCH_RADIUS;
+export const UBER_URL = process.env.UBER_URL;
+export const UBER_TOKEN = process.env.UBER_TOKEN;
